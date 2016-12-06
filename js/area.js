36355f7c3e13fae0e552d28536aeb7b5d96522ce
chevendrasoujanya <kesar989@gmail.com>
2016-12-06 15:01:44 +0530

    Add files via upload

---
+++ js/area.js
@@ -0,0 +1,8 @@
+var area;
+// Function Definition to Find the Area of a Circle
+function calculateArea(radius) {
+	area = 22/7 * radius * radius;
+	return area;
+}
+
+console.log(calculateArea(7)); // Function call in an output statement
